439c9bdaa108251d6d866bbae63b3101dd8a740e
bqbnakasprv7aqe6e <90489496+bqbnakasprv7aqe6e@users.noreply.github.com>
2021-09-16 09:46:14 -0400

    added script file

--- thing.sh
+++ thing.sh
@@ -2,9 +2,10 @@
 
 git filter-branch --env-filter '
 
-OLD_EMAIL="71267505+swannyswan@users.noreply.github.com"
+OLD_EMAIL="bqbnakasprv7aqe6e@gmail.com"
 CORRECT_NAME="bqbnakasprv7aqe6e"
-CORRECT_EMAIL="bqbnakasprv7aqe6e@gmail.com"
+CORRECT_EMAIL="90489496+bqbnakasprv7aqe6e@users.noreply.github.com"
+
 
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
